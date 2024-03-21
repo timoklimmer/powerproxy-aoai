18978951ad1fd33a744d186734fcb779619c3ff3
Timo Klimmer <25802428+timoklimmer@users.noreply.github.com>
2024-03-21 08:22:47 +0100

    Documentation update

--- app/powerproxy.py
+++ app/powerproxy.py
@@ -224,7 +224,8 @@ async def handle_request(request: Request, path: str):
             continue
 
         # replace API key against real API key from AOAI, but only if the request has an API key
-        # note: intentionally not raising an exception here to support Azure AD/Entra ID auth
+        # note: intentionally not raising an exception here to support requests using Azure AD/
+        #       Entra ID authentication.
         if "api-key" in headers:
             headers["api-key"] = aoai_endpoint["key"] or ""
 
